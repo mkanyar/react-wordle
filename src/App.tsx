4939117039a11564a4509d8278202fc2062ecd5e
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2022-03-30 23:04:34 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
 import { Grid } from './components/grid/Grid'
 import { Keyboard } from './components/keyboard/Keyboard'
 import { InfoModal } from './components/modals/InfoModal'
@@ -73,7 +74,11 @@ function App() {
     const loaded = loadGameStateFromLocalStorage()
     if (loaded?.solution !== solution) {
       localStorage.removeItem('saved')
-      localStorage.setItem('startTime', new Date().toISOString())
+      // localStorage.setItem('startTime', new Date().toISOString())
+      localStorage.setItem(
+        'startTime',
+        dayjs().format('YYYY-MM-DDTHH:mm:ssZ[Z]')
+      )
       return []
     }
     const gameWasWon = loaded.guesses.includes(solution)
@@ -87,7 +92,10 @@ function App() {
       })
     }
     if (!localStorage.getItem('startTime')) {
-      localStorage.setItem('startTime', new Date().toISOString())
+      localStorage.setItem(
+        'startTime',
+        dayjs().format('YYYY-MM-DDTHH:mm:ssZ[Z]')
+      )
     }
     return loaded.guesses
   })
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import dayjs from 'dayjs'
 
 import {
   COUNTRY_ENDPOINT,
@@ -11,34 +12,51 @@ type CompletedGamePayload = {
   guesses: string[]
   solution: string
   won: boolean
-  start_time: Date
-  end_time: Date
+  start_time: string
+  end_time: string
   time_taken: number
   country: string
 }
+const customParseFormat = require('dayjs/plugin/customParseFormat')
 
 export const saveGameStateToDatabase = (won: boolean) => {
   const game = loadGameStateFromLocalStorage()
-  const endTime = new Date()
-  const startTime = new Date(localStorage.getItem('startTime') as string)
+  // const endTime = new Date()
+  const endTime = dayjs() //.format('YYYY-MM-DDTHH:mm:ssZ[Z]')
+  // const startTime = new Date(localStorage.getItem('startTime') as string)
+  // const startTime = dayjs(localStorage.getItem('startTime') as string)
+  // const startTime = dayjs(
+  //   localStorage.getItem('startTime') as string,
+  //   ['YYYY-MM-DDTHH:mm:ssZ[Z]'],
+  //   true
+  // )
+  dayjs.extend(customParseFormat)
+  console.log(localStorage.getItem('startTime') as string)
+  // 2022-03-30T15:19:30+02:00Z
+  // const startTime = dayjs('2018-04-04T16:00:00.000Z')
+  const startTime = dayjs(
+    localStorage.getItem('startTime') as string,
+    'YYYY-MM-DDTHH:mm:ssZ[Z]'
+  )
   const completedGame = {
     guesses: game && game.guesses,
     solution: game && game.solution,
     won: won,
-    start_time: startTime,
-    end_time: endTime,
-    time_taken: Math.round((endTime.getTime() - startTime.getTime()) / 1000),
+    start_time: startTime.format('YYYY-MM-DDTHH:mm:ssZ[Z]'),
+    end_time: endTime.format('YYYY-MM-DDTHH:mm:ssZ[Z]'),
+    // time_taken: Math.round((endTime.getTime() - startTime.getTime()) / 1000),
+    time_taken: endTime.diff(startTime, 'second'),
   } as CompletedGamePayload
 
-  if (!localStorage.getItem('saved')) {
-    axios.get(COUNTRY_ENDPOINT).then((res) => {
-      completedGame.country = res.data.country_name
-      axios.post(GAMES_ENDPOINT, completedGame).then(() => {
-        localStorage.setItem('saved', 'true')
-        localStorage.removeItem('startTime')
-      })
+  // if (!localStorage.getItem('saved')) {
+  axios.get(COUNTRY_ENDPOINT).then((res) => {
+    completedGame.country = res.data.country_name
+    axios.post(GAMES_ENDPOINT, completedGame).then(() => {
+      localStorage.setItem('saved', 'true')
+      localStorage.removeItem('startTime')
     })
-  }
+  })
+  // }
 }
 
 export const saveCurrentGuessToDatabase = (currentGuess: string) => {
